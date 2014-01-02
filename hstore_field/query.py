9192e2045396f2fa7b938c127a2353d5a79e62bf
Fernando Gutierrez <fernando.gutierrez@gmail.com>
2014-01-02 11:39:36 -0500

    PEP8 for query.py

--- hstore_field/query.py
+++ hstore_field/query.py
@@ -5,13 +5,14 @@ from django.utils import tree
 from django.core.exceptions import FieldError
 try:
     from django.db.models.sql.constants import LOOKUP_SEP
-except:
+except ImportError:
     from django.db.models.constants import LOOKUP_SEP
 
 
 class HStoreConstraint():
 
-    value_operators = {'exact': '=', 'iexact': '=', 'in': 'IN', 'lt': '<', 'lte': '<=', 'gt': '>', 'gte': '>='}
+    value_operators = {'exact': '=', 'iexact': '=', 'in': 'IN',
+                       'lt': '<', 'lte': '<=', 'gt': '>', 'gte': '>='}
 
     def __init__(self, alias, field, value, lookup_type, key=None):
 
@@ -50,7 +51,8 @@ class HStoreConstraint():
             else:
                 raise ValueError('invalid value %r' % test_value)
             if cast_type:
-                self.lvalue = "CAST(NULLIF(%%s->'%s','') AS %s)" % (key, cast_type)
+                self.lvalue = "CAST(NULLIF(%%s->'%s','') AS %s)" % (key,
+                                                                    cast_type)
             elif lookup_type == 'iexact':
                 self.lvalue = "lower(%%s->'%s')" % key
                 self.values = [value.lower()]
@@ -79,7 +81,8 @@ class HQ(tree.Node):
     AND = 'AND'
     OR = 'OR'
     default = AND
-    query_terms = ['exact', 'iexact', 'lt', 'lte', 'gt', 'gte', 'in', 'contains']
+    query_terms = ['exact', 'iexact', 'lt', 'lte', 'gt', 'gte',
+                   'in', 'contains']
 
     def __init__(self, **kwargs):
         super(HQ, self).__init__(children=kwargs.items())
@@ -121,8 +124,8 @@ class HQ(tree.Node):
                 lookup_type = self.query_terms[0]  # Default lookup type
                 num_parts = len(parts)
                 if len(parts) > 1 and parts[-1] in self.query_terms:
-                    # Traverse the lookup query to distinguish related fields from
-                    # lookup types.
+                    # Traverse the lookup query to distinguish related fields
+                    # from lookup types.
                     lookup_model = query.model
                     for counter, field_name in enumerate(parts):
                         try:
@@ -131,8 +134,8 @@ class HQ(tree.Node):
                             # Not a field. Bail out.
                             lookup_type = parts.pop()
                             break
-                        # Unless we're at the end of the list of lookups, let's attempt
-                        # to continue traversing relations.
+                        # Unless we're at the end of the list of lookups,
+                        # let's attempt to continue traversing relations.
                         if (counter + 1) < num_parts:
                             try:
                                 lookup_model = lookup_field.rel.to
@@ -147,16 +150,21 @@ class HQ(tree.Node):
                     parts = parts[:-1]
                 opts = query.get_meta()
                 alias = query.get_initial_alias()
-                field, target, opts, join_list, last, extra = query.setup_joins(parts, opts, alias, True)
-                col, alias, join_list = query.trim_joins(target, join_list, last, False, False)
-                where_node.add(HStoreConstraint(alias, col, value, lookup_type, key), self.connector)
+                field, target, opts, join_list, last, extra = query.setup_joins(
+                    parts, opts, alias, True)
+                col, alias, join_list = query.trim_joins(
+                    target, join_list, last, False, False)
+                where_node.add(HStoreConstraint(
+                    alias, col, value, lookup_type, key), self.connector)
         if self.negated:
             where_node.negate()
 
 
 def add_hstore(queryset, field, key, name=None):
-    assert queryset.query.can_filter(), "Cannot change a query once a slice has been taken"
+    assert queryset.query.can_filter(), "Cannot change a query once a slice " \
+                                        "has been taken"
     name = name or key
     clone = queryset._clone()
-    clone.query.add_extra({name: "%s -> '%s'" % (field, key)}, None, None, None, None, None)
+    clone.query.add_extra({name: "%s -> '%s'" % (field, key)},
+                          None, None, None, None, None)
     return clone
